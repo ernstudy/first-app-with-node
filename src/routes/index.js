4a7b5b27323e7033a77846dff782dc18fed49a00
ernstudy <hey@ernstudy.com>
2024-04-26 20:42:47 -0600

    First commit

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) =>
+  res.render("index", { title: "First website with nodejs" })
+);
+router.get("/about", (req, res) => res.render("about", { title: "About Me" }));
+
+router.get("/contact", (req, res) =>
+  res.render("contact", { title: "Contact Me" })
+);
+
+export default router;
